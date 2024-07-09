3fb99365c04891776971f6076479d64130bfe515
taiwbi <admin@taiwbi.com>
2024-07-09 17:18:32 +0330

    Removed unneeded configurations on GNOME

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,6 @@
 set -gx EDITOR '/usr/bin/nvim'
 set -gx OPENAI_API_KEY 'null' # Nothing for now :)
+set -gx SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/gcr/ssh"
 
 starship init fish | source
 source $HOME/.config/fish/proxy.fish
