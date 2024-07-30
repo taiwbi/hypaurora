426530806d15d2c6d1d79a308594e524124b1485
taiwbi <admin@taiwbi.com>
2024-07-30 09:12:09 +0330

    Added script to toggle proxy & for rescue background

--- scripts/link.sh
+++ scripts/link.sh
@@ -25,5 +25,7 @@ rm -rf "$HOME/.config/tmux" && ln -s "$PWD/tmux" "$HOME/.config/"
 rm -rf "$HOME/.config/fish" && ln -s "$PWD/fish" "$HOME/.config/"
 rm -rf "$HOME/.config/starship.toml" && ln -s "./fish/starship.toml" "$HOME/.config/starship.toml"
 rm -rf "$HOME/.vimrc" && ln -s "$PWD/vimrc" "$HOME/.vimrc"
+rm -rf "$HOME/.local/bin/toggle-proxy" && ln -s "$PWD/bin/toggle-proxy" "$HOME/.local/bin/toggle-proxy"
+rm -rf "$HOME/.local/bin/toggle-background" && ln -s "$PWD/bin/toggle-background" "$HOME/.local/bin/toggle-background"
 
 echo -e ""
