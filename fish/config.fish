ac61e54641d6a8f347005ad0ade8aae265b1ac9b
taiwbi <admin@taiwbi.com>
2024-07-30 09:13:07 +0330

    Dark and Light Theme for starfish

--- fish/config.fish
+++ fish/config.fish
@@ -2,13 +2,18 @@ set -gx EDITOR '/usr/bin/nvim'
 set -gx OPENAI_API_KEY 'null' # Nothing for now :)
 set -gx SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/gcr/ssh"
 
+set THEME_MODE $(gsettings get org.gnome.desktop.interface color-scheme)
+if [ "$THEME_MODE" = "'prefer-dark'" ]
+  export STARSHIP_CONFIG="$HOME/.config/fish/starship-dark.toml"
+else
+  export STARSHIP_CONFIG="$HOME/.config/fish/starship.toml"
+end
+
 starship init fish | source
 source $HOME/.config/fish/proxy.fish
 
 for file in $HOME/.config/fish/functions/*.fish
   source "$file"
 end
-
-set PATH $HOME/.local/bin  $PATH
 
 set fish_greeting ''
