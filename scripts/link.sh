321c7440afe5bbed10df85603182ceaa42163739
taiwbi <admin@taiwbi.com>
2024-08-04 09:19:28 +0330

    Added ideavimrc for Jetbrains astronvim keybindings

--- scripts/link.sh
+++ scripts/link.sh
@@ -24,6 +24,7 @@ rm -rf "$HOME/.config/neovide" && ln -s "$PWD/neovide" "$HOME/.config/"
 rm -rf "$HOME/.config/tmux" && ln -s "$PWD/tmux" "$HOME/.config/"
 rm -rf "$HOME/.config/fish" && ln -s "$PWD/fish" "$HOME/.config/"
 rm -rf "$HOME/.vimrc" && ln -s "$PWD/vimrc" "$HOME/.vimrc"
+rm -rf "$HOME/.ideavimrc" && ln -s "$PWD/ideavimrc" "$HOME/.ideavimrc"
 rm -rf "$HOME/.local/bin/toggle-proxy" && ln -s "$PWD/bin/toggle-proxy" "$HOME/.local/bin/toggle-proxy"
 rm -rf "$HOME/.local/bin/toggle-background" && ln -s "$PWD/bin/toggle-background" "$HOME/.local/bin/toggle-background"
 
