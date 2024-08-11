3ca53477fe92c67e3aee4739c8c1a517412acef3
taiwbi <admin@taiwbi.com>
2024-08-11 19:56:53 +0330

    feat: Secure aliases for removing, moving, etc. on fish

--- fish/functions/aliases.fish
+++ fish/functions/aliases.fish
@@ -2,3 +2,9 @@
 
 alias 'idehelper' "php artisan ide-helper:models && php artisan ide-helper:generate && php artisan ide-helper:eloquent && php artisan ide-helper:meta"
 alias vi nvim
+
+alias rm 'rm -i'
+alias mv 'mv -i'
+alias mkdir='mkdir -p'
+alias df='df -h'
+alias free='free -m'
