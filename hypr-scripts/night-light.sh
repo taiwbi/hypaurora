2936eafd3465357aff587362cf66715a9a32d597
taiwbi <admin@taiwbi.com>
2024-08-15 15:49:32 +0330

    Fixed Night Light script

--- hypr-scripts/night-light.sh
+++ hypr-scripts/night-light.sh
@@ -3,12 +3,12 @@
 while true; do
   current_hour=$(date +%H)
   if [ "$current_hour" -ge 20 ] || [ "$current_hour" -lt 5 ]; then
-    if ! pgrep -x "wl-gammarelay-rs" > /dev/null; then
+    if ! pgrep -xf "wl-gammarelay-rs" > /dev/null; then
       wl-gammarelay-rs &
       busctl --user set-property rs.wl-gammarelay / rs.wl.gammarelay Temperature q 2300
     fi
   else
-    if pgrep -x "wl-gammarelay-rs" > /dev/null; then
+    if pgrep -xf "wl-gammarelay-rs" > /dev/null; then
         pkill wl-gammarelay-rs
     fi
   fi
