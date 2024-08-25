f2bfb528620a35fbbd7eb45368247e7b27c3293f
taiwbi <admin@taiwbi.com>
2024-08-25 10:51:12 +0330

    Fish Alises

--- fish/functions/aliases.fish
+++ fish/functions/aliases.fish
@@ -1,5 +1,30 @@
 #!/usr/bin/fish
 
-function vi
-  nvim
+alias 'idehelper' "php artisan ide-helper:models && php artisan ide-helper:generate && php artisan ide-helper:eloquent && php artisan ide-helper:meta"
+alias vi nvim
+
+alias ls 'ls --color --group-directories-first'
+
+alias rm 'rm -i'
+alias mv 'mv -i'
+alias mkdir='mkdir -p'
+alias df='df -h'
+alias free='free -m'
+
+function ipinfo
+  curl --silent "http://ipinfo.io/$argv[1]/json" | jq
+end
+
+function myip
+  curl --silent "http://ip-api.com/json" | jq
+end
+
+function wrnd
+  set number (random 1 $argv[1])
+
+  set integer_result (math "$number" / "4" | awk '{print int($1)}')
+  set remainder_result (math "$number" % "4")
+
+  echo "Integer result: $integer_result"
+  echo "Remainder result: $remainder_result"
 end
