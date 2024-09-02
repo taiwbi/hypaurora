593c9077bcbaef7bf4ec7aa2c81a3dec0231dbb1
taiwbi <admin@taiwbi.com>
2024-09-02 10:17:58 +0330

    refactor: Changed starship prompt and use default terminal colors

--- fish/config.fish
+++ fish/config.fish
@@ -2,12 +2,7 @@ set -gx EDITOR '/usr/bin/nvim'
 set -gx OPENAI_API_KEY 'null' # Nothing for now :)
 set -gx SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/gcr/ssh"
 
-set THEME_MODE $(gsettings get org.gnome.desktop.interface color-scheme)
-if [ "$THEME_MODE" = "'prefer-dark'" ]
-  export STARSHIP_CONFIG="$HOME/.config/fish/starship-dark.toml"
-else
-  export STARSHIP_CONFIG="$HOME/.config/fish/starship.toml"
-end
+set -gx STARSHIP_CONFIG "$HOME/.config/starship.toml"
 
 starship init fish | source
 source $HOME/.config/fish/proxy.fish
@@ -16,7 +11,5 @@ source $HOME/.config/fish/fishmarks/marks.fish
 for file in $HOME/.config/fish/functions/*.fish
   source "$file"
 end
-
-alias 'idehelper' "php artisan ide-helper:models && php artisan ide-helper:generate && php artisan ide-helper:eloquent && php artisan ide-helper:meta"
 
 set fish_greeting ''
