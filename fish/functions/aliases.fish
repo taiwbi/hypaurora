2efe246786237e4e38f89cb025fbc2a5a63f771b
taiwbi <admin@taiwbi.com>
2024-12-20 16:45:53 +0330

    feat(term): Added fish config and updated starship

---
+++ fish/functions/aliases.fish
@@ -0,0 +1,37 @@
+#!/usr/bin/fish
+
+alias 'idehelper' "php artisan ide-helper:models && php artisan ide-helper:generate && php artisan ide-helper:eloquent && php artisan ide-helper:meta"
+alias sens 'sensors && echo -e "\r\rNvidia GPU temp: "(nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader,nounits)"°C"'
+alias vi nvim
+
+alias ls 'lsd --group-directories-first'
+
+alias rm 'rm -i'
+alias mv 'mv -i'
+alias mkdir='mkdir -p'
+alias df='df -h'
+alias free='free -h'
+alias aria='aria2c -x 16'
+
+if test "$TERM" = "xterm-kitty"
+  alias icat='kitty +kitten icat'
+  alias ssh='kitty +kitten ssh'
+end
+
+function ipinfo
+  curl --silent "http://ipinfo.io/$argv[1]/json" | jq
+end
+
+function myip
+  curl --silent "http://ip-api.com/json" | jq
+end
+
+function wrnd
+  set number (random 1 $argv[1])
+
+  set integer_result (math "$number" / "4" | awk '{print int($1)}')
+  set remainder_result (math "$number" % "4")
+
+  echo "Integer result: $integer_result"
+  echo "Remainder result: $remainder_result"
+end
