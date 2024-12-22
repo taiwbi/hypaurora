97af596e752c17a8eb808d83c2856986dbc05012
taiwbi <admin@taiwbi.com>
2024-12-22 18:00:43 +0330

    feat: Attach opened neovim tmux session if exists

--- fish/config.fish
+++ fish/config.fish
@@ -14,9 +14,31 @@ end
 
 set fish_greeting ''
 
+# Open tmux in neovim terminal
 if status is-interactive
   and not set -q argv[1]
   and not set -q TMUX
   and set -q NVIM
-  exec tmux
+
+  # Ensure tmux is installed
+  if not command -v tmux &> /dev/null
+    echo "tmux is not installed. Please install tmux to use this feature."
+    return 1
+  end
+
+  # Set the session name based on the current directory
+  set nv_dir_name (basename (pwd))
+  set nv_session_name "TMX-$nv_dir_name"
+
+  # Check if the tmux session already exists
+  if tmux has-session -t $nv_session_name
+    tmux attach -t $nv_session_name
+  else
+    # Create a new tmux session
+    tmux new -s $nv_session_name 2> /dev/null
+    if test $status -ne 0
+      echo "Failed to create a new tmux session."
+      return 1
+    end
+  end
 end
