0aa6e89a493cbf1c4e9ed72ecd24699a54fc1b8f
taiwbi <admin@taiwbi.com>
2025-01-25 15:46:46 +0330

    fix(config.sh): update monospace font name

--- scripts/config.sh
+++ scripts/config.sh
@@ -19,7 +19,7 @@ fi
 gsettings set org.gnome.desktop.interface cursor-theme 'Adwaita'
 gsettings set org.gnome.desktop.interface gtk-theme "'adw-gtk3'"
 gsettings set org.gnome.desktop.interface font-name 'DIN Pro 11'
-gsettings set org.gnome.desktop.interface monospace-font-name 'PFDinMono-Regular 15'
+gsettings set org.gnome.desktop.interface monospace-font-name 'PFDinMono-Regular, AzarMehrMonospaced 13'
 gsettings set org.gnome.desktop.interface document-font-name 'Addington CF, Aria 12'
 gsettings set org.gnome.desktop.interface text-scaling-factor '1'
 
