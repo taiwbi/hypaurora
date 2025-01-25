fd178b19d1fda68f70201574846abc9686e3144f
taiwbi <admin@taiwbi.com>
2025-01-25 15:45:50 +0330

    style(config.fish): fix indentation for improved readability

--- fish/config.fish
+++ fish/config.fish
@@ -7,7 +7,7 @@ set -gx STARSHIP_CONFIG "$HOME/.config/starship.toml"
 starship init fish | source
 source $HOME/.config/fish/proxy.fish
 if test -f "/etc/grc.fish"
-    source /etc/grc.fish
+  source /etc/grc.fish
 end
 
 for file in $HOME/.config/fish/functions/*.fish
@@ -17,24 +17,24 @@ end
 set fish_greeting ''
 
 if status is-interactive
-    and not set -q argv[1]
-    and not set -q TMUX
-    and set -q NVIM
-    # Ensure tmux is installed
-    if command -v tmux &> /dev/null
-        handle_tmux_session
-    else
-        echo "tmux is not installed. Please install tmux to use this feature."
-    end
+  and not set -q argv[1]
+  and not set -q TMUX
+  and set -q NVIM
+  # Ensure tmux is installed
+  if command -v tmux &> /dev/null
+    handle_tmux_session
+  else
+    echo "tmux is not installed. Please install tmux to use this feature."
+  end
 end
 
 if command -v tput &> /dev/null; and [ -t 1 ]; and [ -n "$TERM" ]; and [ $TERM != "dumb" ]
-    if [ $TERM = "xterm-ghostty" -o $TERM = "xterm-kitty" ];
-        kitten icat --align=center ~/Pictures/2B/main-2.png
-        echo ""
-    end
+  if [ $TERM = "xterm-ghostty" -o $TERM = "xterm-kitty" ];
+    kitten icat --align=center ~/Pictures/2B/main-2.png
+    echo ""
     set text "Beneath her cold exterior lies a warmth that defies her programming,"
     printf "%*s\n" (math "round(($(tput cols) + $(string length "$text")) / 2)") "$text"
     set text "yearning to protect what she cherishes most."
     printf "%*s\n" (math "round(($(tput cols) + $(string length "$text")) / 2)") "$text"
+  end
 end
