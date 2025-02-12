5fa8cebe42c267af591b0a3be9ec52ad860db53f
taiwbi <admin@taiwbi.com>
2025-02-12 11:30:28 +0330

    chore: Removed deprecated scripts

--- hypr-scripts/eyes.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-while true; do
-  sleep 1200
-  notify-send "Eye rest time" "It's time to rest your eyes for 20 seconds, I'll send you another notification when 20s is done."
-  sleep 20
-  notify-send "Get back to work" "Eye rest time is over, continue your work"
-done
