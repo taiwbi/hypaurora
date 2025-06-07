85db5beb0ebcf417295317edba8ffb5902945d0d
taiwbi <taiwbii@proton.me>
2025-06-07 15:15:00 +0330

    reformat

--- scripts/link.sh
+++ scripts/link.sh
@@ -17,19 +17,45 @@ fi
 
 mkdir -p "$HOME/Pictures/background/"
 
-rm -rf "$HOME/.config/starship.toml" && ln -s "$PWD/starship.toml" "$HOME/.config/starship.toml"
-rm -rf "$HOME/.config/fontconfig" && ln -s "$PWD/fontconfig" "$HOME/.config/"
-rm -rf "$HOME/.config/mpv" && ln -s "$PWD/mpv" "$HOME/.config/"
-rm -rf "$HOME/.config/gtk-3.0" && ln -s "$PWD/gtk-3.0" "$HOME/.config/"
-rm -rf "$HOME/.config/gtk-4.0" && ln -s "$PWD/gtk-4.0" "$HOME/.config/"
-rm -rf "$HOME/.config/kitty" && ln -s "$PWD/kitty" "$HOME/.config/"
-rm -rf "$HOME/.config/ghostty" && ln -s "$PWD/ghostty" "$HOME/.config/"
-rm -rf "$HOME/.config/tmux" && ln -s "$PWD/tmux" "$HOME/.config/"
-rm -rf "$HOME/.config/fish" && ln -sf "$PWD/fish" "$HOME/.config/fish"
-rm -rf "$HOME/.config/fastfetch" && ln -sf "$PWD/fastfetch" "$HOME/.config/fastfetch"
-rm -rf "$HOME/.config/hypr" && ln -sf "$PWD/hypr" "$HOME/.config/hypr"
-rm -rf "$HOME/.config/hyprpanel" && ln -sf "$PWD/hyprpanel" "$HOME/.config/hyprpanel"
-rm -rf "$HOME/.config/rofi" && ln -sf "$PWD/rofi" "$HOME/.config/rofi"
+
+rm -rf "$HOME/.config/starship.toml";
+ln -s "$PWD/starship.toml" "$HOME/.config/starship.toml"
+
+rm -rf "$HOME/.config/fontconfig";
+ln -s "$PWD/fontconfig" "$HOME/.config/"
+
+rm -rf "$HOME/.config/mpv";
+ln -s "$PWD/mpv" "$HOME/.config/"
+
+rm -rf "$HOME/.config/gtk-3.0";
+ln -s "$PWD/gtk-3.0" "$HOME/.config/"
+
+rm -rf "$HOME/.config/gtk-4.0";
+ln -s "$PWD/gtk-4.0" "$HOME/.config/"
+
+rm -rf "$HOME/.config/kitty";
+ln -s "$PWD/kitty" "$HOME/.config/"
+
+rm -rf "$HOME/.config/ghostty";
+ln -s "$PWD/ghostty" "$HOME/.config/"
+
+rm -rf "$HOME/.config/tmux";
+ln -s "$PWD/tmux" "$HOME/.config/"
+
+rm -rf "$HOME/.config/fish";
+ln -sf "$PWD/fish" "$HOME/.config/fish"
+
+rm -rf "$HOME/.config/fastfetch";
+ln -sf "$PWD/fastfetch" "$HOME/.config/fastfetch"
+
+rm -rf "$HOME/.config/hypr";
+ln -sf "$PWD/hypr" "$HOME/.config/hypr"
+
+rm -rf "$HOME/.config/hyprpanel";
+ln -sf "$PWD/hyprpanel" "$HOME/.config/hyprpanel"
+
+rm -rf "$HOME/.config/rofi";
+ln -sf "$PWD/rofi" "$HOME/.config/rofi"
 
 if [ -d "$HOME/.local/share/epiphany" ]; then
   rm -f "$HOME/.local/share/epiphany/user-*";
