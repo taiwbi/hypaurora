64af48669e7a0b6a52fcf4aa5cb82255bfaa4025
taiwbi <taiwbii@proton.me>
2025-07-18 10:41:47 +0330

    fix: Disable bracketed paste and focus reporting in fish shell

--- fish/config.fish
+++ fish/config.fish
@@ -13,3 +13,9 @@ end
 for rc in ~/.config/fish/conf.d/*.fish
     source "$rc"
 end
+
+# --- Configuration Options ---
+set -g fish_features '!'bracketed-paste
+
+# disable terminal focus reporting (DEC private mode 1004)
+printf '\e[?1004l'
