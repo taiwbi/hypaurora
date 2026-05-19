48c96498d5b9c024413634b4262f484e1b816747
taiwbi <taiwbii@proton.me>
2026-05-19 09:18:29 +0330

    chore(aliases): Update shell aliases to use command nvim and provide fallback message
    - Replace vi and v aliases with command nvim to ensure external command.
    - Add cvi alias for command nvim --clean.
    - Alias nvim to echo a message encouraging use of vi.

--- fish/conf.d/aliases.fish
+++ fish/conf.d/aliases.fish
@@ -4,9 +4,10 @@
 alias fastfetch 'fastfetch --gpu-hide-type integrated'
 alias ff fastfetch
 
-alias vi nvim
-alias cvi 'nvim --clean'
-alias nvim 'commanddoesnotexists' # Trying to make myself used to vi command
+alias vi 'command nvim'
+alias v 'command nvim'
+alias cvi 'command nvim --clean'
+alias nvim 'echo "Use vi :)"'
 alias cl clear
 alias cd.. 'cd ..'
 alias .. 'cd ..'
