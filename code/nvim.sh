3de160cd945298496705ff2de890ef1da87e6365
taiwbi <taiwbii@proton.me>
2026-08-06 20:55:53 +0330

    fix(nvim): use neovide from PATH
    Replace the hardcoded local Neovide binary path with the
    PATH-resolved executable.

--- code/nvim.sh
+++ code/nvim.sh
@@ -53,5 +53,5 @@ elif [[ "$1" == "ghostty" ]]; then
     shift
     ghostty --gtk-titlebar=false --class="org.nvim.neovide" -e /bin/bash -c "sleep .1 && nvim" -- "$@"
 else
-    "$HOME/.local/binary/neovide" --wayland_app_id "org.nvim.neovide" --neovim-bin "nvim"
+    "neovide" --wayland_app_id "org.nvim.neovide" --neovim-bin "nvim"
 fi
