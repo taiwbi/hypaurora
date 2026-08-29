f4e8e03afb8a6a27ca14a6a63a74715028173c54
taiwbi <taiwbii@proton.me>
2026-08-29 15:21:31 +0330

    feat(shell): include Composer and tool paths in PATH

--- code/nvim.sh
+++ code/nvim.sh
@@ -44,7 +44,7 @@ set_proxy
 
 export PHP_CS_FIXER_IGNORE_ENV=true
 
-export PATH="$HOME/.local/bin:$HOME/.local/npm/bin:$PATH"
+export PATH="$HOME/.local/bin:$HOME/.local/npm/bin:$HOME/.cargo/bin:$HOME/.config/composer/vendor/bin:$HOME/.npm-global/bin:$PATH"
 
 if [[ "$1" == "kitty" ]]; then
     shift
