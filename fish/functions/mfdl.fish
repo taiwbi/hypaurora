85616ddfe3b829dcd05a8a6338af0de74d41adfa
taiwbi <taiwbii@proton.me>
2026-09-04 11:03:20 +0330

    fix(mfdl): disable grep color output to fix link extraction
    Add --color=never to grep calls so color escape codes don't break
    download link extraction. Also follow redirects with curl -L in the
    bash version.

--- fish/functions/mfdl.fish
+++ fish/functions/mfdl.fish
@@ -6,7 +6,11 @@ function mfdl
 
     set url $argv[1]
 
-    set direct_url (curl -s $url | grep -Eo 'https://download[0-9]*\.mediafire\.com/[^"]+' | head -1)
+    set direct_url (
+        curl -s $url| 
+        grep --color=never -Eo 'https://download[0-9]*\.mediafire\.com/[^"]+' |
+        head -1
+    )
 
     if test -z "$direct_url"
         echo "Error: Could not extract download link from $url"
